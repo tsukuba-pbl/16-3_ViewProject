f4893e730fc5c6338fb14865b6241ad019e0089b
Minajun <s1620678@u.tsukuba.ac.jp>
2016-11-05 15:09:05 +0900

    サーバから全天球画像と座標などのJSONを取得し，パースする処理を実装

---
+++ ViewProject/Assets/Scripts/RoomLayout.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using MiniJSON;
+using System.Collections.Generic;
+
+public class RoomLayout : MonoBehaviour {
+
+	private string path = "https://salty-fortress-26407.herokuapp.com/uxsers/9.json";
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		using(WWW www = new WWW(path)){
+
+			yield return www;
+
+			if(!string.IsNullOrEmpty(www.error)){
+
+				Debug.LogError("www Error:" + www.error);
+				yield break;
+
+			}
+
+			Debug.Log(www.text);
+			var jsonDict = Json.Deserialize(www.text) as Dictionary<string,object>;
+			Debug.Log("id = " + jsonDict["id"]);
+			Debug.Log("gazo_url = " + jsonDict["gazo_url"]);
+			var jsonDict2 = jsonDict ["zahyo"] as Dictionary<string,object>;
+			Debug.Log("x = " + jsonDict2["x"]);
+			Debug.Log("y = " + jsonDict2["y"]);
+			Debug.Log("z = " + jsonDict2["z"]);
+			Debug.Log("offset = " + jsonDict["offset"]);
+		}
+
+
+	}
+}
