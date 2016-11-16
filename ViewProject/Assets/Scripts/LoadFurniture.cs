9bdc795bb770e96c1acc82cf489d0d1832a8ae5e
Jun Minagawa <s1620678@u.tsukuba.ac.jp>
2016-11-16 18:59:13 +0900

    受信したJSONデータから，家具の種類・位置・角度を取得する

--- ViewProject/Assets/Scripts/LoadFurniture.cs
+++ ViewProject/Assets/Scripts/LoadFurniture.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using MiniJSON;
+using System.Collections.Generic;
 
 public class LoadFurniture : MonoBehaviour {
 	private string path = "https://salty-fortress-26407.herokuapp.com/layouts/1.json";
@@ -18,7 +20,33 @@ public class LoadFurniture : MonoBehaviour {
 
 			}
 
-			Debug.Log(www.text);
+			//Debug.Log(www.text);
+
+			//受信した家具配置情報をパースする
+			var jsonDict = Json.Deserialize(www.text) as Dictionary<string,object>;
+			var jsonDict2 = jsonDict["contents"] as  Dictionary<string,object>;
+			int idLayout = int.Parse(jsonDict2 ["layout_id"].ToString ()); //レイアウトIDの格納
+			//Debug.Log (idLayout);
+
+			//家具の種類と座標をパースし，配置する
+			List<object> list = (List<object>)jsonDict2 ["placed_furniture_items"];
+			for (int i = 0; i < list.Count; i++) {
+				var test = ((List<object>)jsonDict2 ["placed_furniture_items"]) [i] as Dictionary<string,object>;
+				//回転成分の格納
+				float a = float.Parse(test ["a_rotation"].ToString());
+				float b = float.Parse(test ["b_rotation"].ToString());
+				float c = float.Parse(test ["c_rotation"].ToString());
+				Debug.Log ("Rotation = (" + a + "," + b + "," + c + ")");
+				//座標成分の格納
+				float x = float.Parse(test ["x_coordinate_data"].ToString());
+				float y = float.Parse(test ["y_coordinate_data"].ToString());
+				float z = float.Parse(test ["z_coordinate_data"].ToString());
+				Debug.Log ("Position = (" + x + "," + y + "," + z + ")");
+				//家具の種別の格納
+				int furniture_item_id = int.Parse(test ["furniture_item_id"].ToString());
+				Debug.Log ("item id = " + furniture_item_id);
+			}
+
 			/*
 			var jsonDict = Json.Deserialize(www.text) as Dictionary<string,object>;
 			Debug.Log("id = " + jsonDict["id"]);
