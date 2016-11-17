9d8f25e8497961c5d09e017f63fc6e732edfea00
Minajun <s1620678@u.tsukuba.ac.jp>
2016-11-17 22:56:31 +0900

    家具配置情報から，家具を指定位置に配置する処理を実装

--- ViewProject/Assets/Scripts/LoadFurniture.cs
+++ ViewProject/Assets/Scripts/LoadFurniture.cs
@@ -45,31 +45,39 @@ public class LoadFurniture : MonoBehaviour {
 				//家具の種別の格納
 				int furniture_item_id = int.Parse(test ["furniture_item_id"].ToString());
 				Debug.Log ("item id = " + furniture_item_id);
+				//家具のロードと配置
+				GameObject instance = null;
+				string furniture_name = null;
+				switch (furniture_item_id) {
+				case 1:
+					furniture_name = "bed1";
+					break;
+				case 2:
+					furniture_name = "bed2";
+					break;
+				case 3:
+					furniture_name = "Chair";
+					break;
+				case 4:
+					furniture_name = "Glass_table";
+					break;
+				case 5:
+					furniture_name = "sek";
+					break;
+				case 6:
+					furniture_name = "table";
+					break;
+				default:
+					furniture_name = null;
+					break;
+				}
+				if (furniture_name != null) {
+					instance = Instantiate (Resources.Load (furniture_name)) as GameObject;
+					Vector3 p = new Vector3 (x, y, z);
+					instance.transform.position = p;
+					instance.transform.rotation = Quaternion.Euler(a, b, c);
+				}
 			}
-
-			/*
-			var jsonDict = Json.Deserialize(www.text) as Dictionary<string,object>;
-			Debug.Log("id = " + jsonDict["id"]);
-			Debug.Log("gazo_url = " + jsonDict["gazo_url"]);
-			url = jsonDict ["gazo_url"].ToString ();
-
-			var jsonDict2 = jsonDict ["zahyo"] as Dictionary<string,object>;
-			Debug.Log("x = " + jsonDict2["x"]);
-			Debug.Log("y = " + jsonDict2["y"]);
-			Debug.Log("z = " + jsonDict2["z"]);
-			Debug.Log("offset = " + jsonDict["offset"]);
-
-			//カメラの位置を変更
-			float x = float.Parse(jsonDict2["x"].ToString());
-			float y = float.Parse(jsonDict2["y"].ToString());
-			float z = float.Parse(jsonDict2 ["z"].ToString());
-			Vector3 cameraPos = new Vector3(x, y, z);
-			SetCameraPos (cameraPos);
-
-			//全天球画像の表示
-			GameObject instance = Instantiate (Resources.Load ("SphereMovie")) as GameObject;
-			instance.transform.position = cameraPos;
-			*/
 		}
 	}
 }
--- ViewProject/Assets/Scripts/RoomLayout.cs
+++ ViewProject/Assets/Scripts/RoomLayout.cs
@@ -41,12 +41,12 @@ public class RoomLayout : MonoBehaviour {
 			float x = float.Parse(jsonDict2["x"].ToString());
 			float y = float.Parse(jsonDict2["y"].ToString());
 			float z = float.Parse(jsonDict2 ["z"].ToString());
-			Vector3 cameraPos = new Vector3(x, y, z);
-			SetCameraPos (cameraPos);
+			//Vector3 cameraPos = new Vector3(x, y, z);
+			//SetCameraPos (cameraPos);
 
 			//全天球画像の表示
 			GameObject instance = Instantiate (Resources.Load ("SphereMovie")) as GameObject;
-			instance.transform.position = cameraPos;
+			//instance.transform.position = cameraPos;
 		}
 	}
 
