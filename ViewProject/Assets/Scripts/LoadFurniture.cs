c6acb8ba318ce6ed7c94f59fcf7219b4b3b2f611
Minajun <s1620678@u.tsukuba.ac.jp>
2017-01-23 22:16:13 +0900

    配置可能家具にベッドを一つ追加

--- ViewProject/Assets/Scripts/LoadFurniture.cs
+++ ViewProject/Assets/Scripts/LoadFurniture.cs
@@ -97,6 +97,9 @@ public class LoadFurniture : MonoBehaviour {
 					case 6:
 						furniture_name = "sek";
 						break;
+					case 7:
+						furniture_name = "bed3";
+						break;
 					default:
 						furniture_name = null;
 						break;
